3cdc1c0faac5aea4b62bd1d72e570eed87dfd5e6
Karl Denby <karl.denby@gmail.com>
2016-10-18 11:36:56 +0100

    implement layout

--- app/src/main/java/com/example/android/pic_upload_test/MainActivity.java
+++ app/src/main/java/com/example/android/pic_upload_test/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.pic_upload_test;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView txtResourceId = (TextView) findViewById(R.id.main_tv_resourceId);
+        txtResourceId.setText(getString(R.string.res_id_is, 12345));
     }
 }
