935fdf65f18de3800774b80cf6c6631b8985c731
Karl Denby <karl.denby@gmail.com>
2016-10-19 12:27:24 +0100

    photo selection intent is called when users clicks upload

--- app/src/main/java/com/example/android/pic_upload_test/MainActivity.java
+++ app/src/main/java/com/example/android/pic_upload_test/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.android.pic_upload_test;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_OPEN = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +22,29 @@ public class MainActivity extends AppCompatActivity {
 
         TextView txtResourceId = (TextView) findViewById(R.id.main_tv_resourceId);
         txtResourceId.setText(getString(R.string.res_id_is, 12345));
+
+        Button btnUpload = (Button) findViewById(R.id.main_btn_upload);
+        btnUpload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selectImage();
+            }
+        });
+
     }
+
+    public void selectImage() {
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+        intent.setType("image/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        startActivityForResult(intent, REQUEST_IMAGE_OPEN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_OPEN && resultCode == RESULT_OK) {
+            Uri fullPhotoUri = data.getData();
+        }
+    }
+
 }
