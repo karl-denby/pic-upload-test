c6544f96a6cd6e8b141e46bd785297612d8d52fa
Karl Denby <karl.denby@gmail.com>
2016-10-20 14:43:39 +0100

    get actual resolvable content uri, Uri.toString() instead of Uri.getPath().toString

--- app/src/main/java/com/example/android/pic_upload_test/MainActivity.java
+++ app/src/main/java/com/example/android/pic_upload_test/MainActivity.java
@@ -5,12 +5,16 @@ import android.net.Uri;
 import android.support.v4.app.ShareCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import java.io.File;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,7 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         TextView txtURI = (TextView) findViewById(R.id.main_tv_URI);
-        txtURI.setText(getString(R.string.uri_is, "/data/"));
+        txtURI.setText(getString(R.string.uri_is, "No image"));
 
         Button btnUpload = (Button) findViewById(R.id.main_btn_upload);
         btnUpload.setOnClickListener(new View.OnClickListener() {
@@ -31,7 +35,6 @@ public class MainActivity extends AppCompatActivity {
                 selectImage();
             }
         });
-
     }
 
     public void selectImage() {
@@ -45,10 +48,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == REQUEST_IMAGE_OPEN && resultCode == RESULT_OK) {
             Uri fullPhotoUri = data.getData();
+            String filePath = fullPhotoUri.toString();
 
+            // set string
             TextView txtURI = (TextView) findViewById(R.id.main_tv_URI);
-            txtURI.setText(getString(R.string.uri_is, fullPhotoUri.getPath()));
+            txtURI.setText(getString(R.string.uri_is, filePath ));
 
+            // set image
             ImageView ivSelectedFile = (ImageView) findViewById(R.id.main_iv_SelectedFile);
             ivSelectedFile.setImageURI(fullPhotoUri);
         }
