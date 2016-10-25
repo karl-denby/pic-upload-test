f896748fdfa731efc826ea4bd5c726b13bca707b
Karl Denby <karl.denby@gmail.com>
2016-10-25 15:17:02 +0100

    remove url hacks, load image into encoded string, decode and display, next step push this into a database

--- app/src/main/java/com/example/android/pic_upload_test/MainActivity.java
+++ app/src/main/java/com/example/android/pic_upload_test/MainActivity.java
@@ -2,19 +2,25 @@ package com.example.android.pic_upload_test;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Base64;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.io.ByteArrayOutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     static final int REQUEST_IMAGE_OPEN = 1;
+    Bitmap bmp = null;
+    String encodedBmp = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,16 +39,16 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        String loc = "content://media/external/images/media/19";  //pre-kitkat uri
-        Bitmap bmp = null;
+        String loc = "content://media/external/images/media/247";  //pre-kitkat uri
         try {
             bmp = MediaStore.Images.Media.getBitmap(this.getContentResolver(), Uri.parse(loc));
+            encodedBmp = encodeToBase64(bmp, Bitmap.CompressFormat.JPEG, 100);
         } catch (Exception e) {
             Log.v("bmp Exception", "Can't make uri string into a bitmap: " + e.toString());
         }
         // set string and image
         txtURI.setText(getString(R.string.uri_is, loc));
-        ivSelectedFile.setImageBitmap(bmp);
+        ivSelectedFile.setImageBitmap(decodeBase64(encodedBmp));
     }
 
     public void selectImage() {
@@ -59,32 +65,30 @@ public class MainActivity extends AppCompatActivity {
             // Get URI
             // to String
             Uri fullPhotoUri = data.getData();
-            String filePath = convertUri(Uri.decode(fullPhotoUri.toString()));
 
             // set string on txtView
             TextView txtURI = (TextView) findViewById(R.id.main_tv_URI);
-            txtURI.setText(getString(R.string.uri_is, filePath ));  // use string for textbox
+            txtURI.setText(getString(R.string.uri_is, fullPhotoUri.toString()));  // use string for textbox
 
             // set image using string version parsed as a URI
             ImageView ivSelectedFile = (ImageView) findViewById(R.id.main_iv_SelectedFile);
-            Log.v("URI",filePath);
-            ivSelectedFile.setImageURI(Uri.parse(filePath)); // convert string back to URI
+            ivSelectedFile.setImageURI(fullPhotoUri); // convert string back to URI
         }
     }
 
-    private String convertUri(String badUri) {
-        String goodUri = "content://media/external/images/media/";
-
-        // First run gives us up to ':' after content
-        int startImagePosition = badUri.indexOf(":") + 1;
-        int endImagePosition = badUri.length();
-        String imageNumber = badUri.substring(startImagePosition, endImagePosition);
-
-        // Second run should give us the ':' we want
-        startImagePosition = imageNumber.indexOf(":") + 1;
-        endImagePosition = imageNumber.length();
-        imageNumber = imageNumber.substring(startImagePosition, endImagePosition);
-
-        return goodUri + imageNumber;
+    public static String encodeToBase64(Bitmap image, Bitmap.CompressFormat compressFormat, int quality)
+    {
+        ByteArrayOutputStream byteArrayOS = new ByteArrayOutputStream();
+        image.compress(compressFormat, quality, byteArrayOS);
+        return Base64.encodeToString(byteArrayOS.toByteArray(), Base64.DEFAULT);
     }
+
+    public static Bitmap decodeBase64(String input)
+    {
+        byte[] decodedBytes = Base64.decode(input, 0);
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.length);
+    }
+
+    // String myBase64Image = encodeToBase64(myBitmap, Bitmap.CompressFormat.JPEG, 100);
+    // Bitmap myBitmapAgain = decodeBase64(myBase64Image);
 }
